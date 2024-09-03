2564b088ce4fdcfaba8a6a03da48a3521af98938
RhythmAgg <aggarwalrhythm2211@gmail.com>
2024-09-03 20:48:34 +0530

    default buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,32 @@
+import { useState } from 'react'
+import './App.css'
+import PrimaryNoIcon from './Components/Buttons/PrimaryNoIcon'
+import PrimaryWithIcon from './Components/Buttons/PrimaryWithIcon'
+import SecondaryNoIcon from './Components/Buttons/SecondaryNoIcon'
+import SecondaryWithIcon from './Components/Buttons/SecondaryWithIcon'
+import HomeIcon from './Assets/Icons/home.svg'
+import HomeBlueIcon from './Assets/Icons/HomeBlue.svg'
+
 function App() {
-  return <></>;
+
+  return (
+    <div className='flex gap-x-4'>  
+      <PrimaryNoIcon 
+        content='Button'
+      />
+      <PrimaryWithIcon
+        content='Button'
+        iconSrc={HomeIcon}
+      />
+      <SecondaryNoIcon
+        content='Button'
+      />
+      <SecondaryWithIcon
+        content='Button'
+        iconSrc={HomeBlueIcon}
+      />
+    </div>
+  )
 }
 
-export default App;
+export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,43 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    screens: {
+      'mobile': '340px',
+
+      'tablet': '640px',
+
+      'laptop': '1024px',
+
+      'desktop': '1280px',
+    },
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      colors: {
+        CustomText: {
+          300: '#292929',
+          200: '#757575',
+          100: '#ffffff',
+        },
+        CustomBrand: {
+          300: '#3B82F6',
+          200: '#C3DAFF',
+          100: '#EBF3FE'
+        },
+        CustomNeutral: {
+          400: '#292929',
+          300: '#BFBFBF',
+          200: '#EF1F1F1',
+          100: '#FFFFFF'
+        }
+      },
+    },
   },
   plugins: [],
-};
+}
+
