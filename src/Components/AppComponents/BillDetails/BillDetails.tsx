56374adc3b150dc381ff8285c8bc39ea34d3be14
RhythmCyfrone <rhythm.aggarwal@cyfrone.com>
2024-09-08 19:15:48 +0530

    bill animations primary

--- src/Components/AppComponents/BillDetails/BillDetails.tsx
+++ src/Components/AppComponents/BillDetails/BillDetails.tsx
@@ -9,9 +9,11 @@ import CancelBillIcon from '../../../Assets/Icons/x-button.svg'
 import MoreIcon from '../../../Assets/Icons/More.svg'
 import '../../../Styles/Fonts.css'
 import '../../../Styles/Shadows.css'
+import '../../../Styles/Animations.css'
 
 const BillDetails = () => {
     const [isRotated, setIsRotated] = useState(false);
+    const [viewBill, setViewBill] = useState(false)
     const [viewMore, setViewMore] = useState(false);
     
     const bottomPadding = isRotated?'pb-[16px]':'pb-[32px]'
@@ -20,22 +22,58 @@ const BillDetails = () => {
 
     const handleIconClick = () => {
         setIsRotated(!isRotated);
+        if(!viewBill)
+            setViewBill(true)
     };
   return (
-    <div className='absolute bottom-[0px]'>
-        <div className={`relative w-[420px] bg-[#DDEBFF] rounded-t-[32px] flex flex-col ${topPadding}`}>
-            <div className={`absolute left-[50%] top-[0px] ${translate} -translate-x-[50%] w-[147px] h-[80px] bg-[#DDEBFF] rounded-t-[16px]
+    <div className='absolute bottom-[0px] z-10'>
+        <div className={`relative w-[420px] bg-[#DDEBFF] rounded-t-[32px] flex flex-col transform-padding duration-500 ${topPadding}`}>
+            {viewBill && 
+                <div className={`ps-[32px] pe-[32px] flex flex-col ${isRotated?'fade-in ':'fade-out'}`} 
+                    onAnimationEnd={() => {
+                        if(!isRotated)
+                            setViewBill(false)
+                    }}
+                >
+                    <div className='flex justify-between p-0 items-center'>
+                        <span className='font-poppins font-normal text-[16px]'>Sub Total</span>
+                        <span className='font-poppins font-normal text-[20px]'>1000 /-</span>
+                    </div>
+                    <div className='flex flex-col gap-y-[4px]'>
+                        <div className='flex justify-between p-0 items-center'>
+                            <span className='font-poppins font-normal text-[16px]'>Discount (10%)</span>
+                            <span className='font-poppins font-normal text-[16px] text-[#FF4F4F] cursor-pointer'>Remove</span>
+                            <span className='font-poppins font-normal text-[12px]'>171</span>
+                        </div>
+                        <div className='flex justify-between p-0 items-center'>
+                            <span className='font-poppins font-normal text-[16px]'>Service Charge (2%)</span>
+                            <span className='font-poppins font-normal text-[16px] text-[#FF4F4F] cursor-pointer'>Remove</span>
+                            <span className='font-poppins font-normal text-[12px]'>34.22</span>
+                        </div>
+                        <div className='flex justify-between p-0 items-center'>
+                            <span className='font-poppins font-normal text-[16px]'>SGST (13%)</span>
+                            <span className='font-poppins font-normal text-[12px]'>50.6</span>
+                        </div>
+                        <div className='flex justify-between mb-[16px] p-0 items-center'>
+                            <span className='font-poppins font-normal text-[16px]'>CGST (13%)</span>
+                            <span className='font-poppins font-normal text-[12px]'>90.71</span>
+                        </div>
+                    </div>
+                    
+                </div>
+            }
+            <div className={`absolute left-[50%] top-[0px] ${translate} -translate-x-[50%] transform-all duration-500 w-[147px] min-h-[80px] bg-[#DDEBFF] rounded-t-[16px]
                             flex flex-col items-center p-[8px]`}
             >
                 <img
                     src={BillViewIcon}
-                    className={`cursor-pointer transition-transform duration-300 ${
-                        isRotated ? '-rotate-180' : ''
+                    className={`cursor-pointer transition-transform duration-500 ${
+                        isRotated ? '-rotate-180':''
                     }`}
                     onClick={handleIconClick}
                 />
                 {
-                    isRotated
+                    isRotated 
                     ?<span className='font-normal text-[14px] text-[#020202]'>Close</span>
                     :<span className='font-normal text-[14px] text-[#020202]'>View Bill Details</span>
                 }
@@ -75,7 +113,7 @@ const BillDetails = () => {
                     <span className='font-poppins font-normal text-[8.5px] text-white'>Cancel Order</span>
                 </div>
             </div>
-            <div className={`ps-[32px] pe-[32px] w-full mt-[12px] ${bottomPadding} transition-all duration-300`}>
+            <div className={`ps-[32px] pe-[32px] w-full mt-[12px] ${bottomPadding} transition-all duration-500`}>
                 <div className='text-center fontButtonDefault bg-CustomBrand-300 cursor-pointer rounded-[8px] p-[8px] ps-[24px] pe-[24px] hover:ShadowPrimaryHover active:shadow-none'
                 >
                     Generate Bill
